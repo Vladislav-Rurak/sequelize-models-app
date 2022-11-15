81a5c03b031b694efc4073e37fbccf9176d798c2
bbd <dolgkozly@gmail.com>
2022-11-15 23:21:13 +0200

    Add .sequelizerc

---
+++ config/db.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    database: process.env.DB_NAME ?? 'users_db_dev',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
